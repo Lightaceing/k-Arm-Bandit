Corrected Incremental Average Formula

src/enviroment.py
@@ -2,10 +2,11 @@ import random
 import numpy as np
 
 
-def create_enviroment(n_arms : int, max_mean = 10):
+def create_enviroment(n_arms : int, max_mean = 10, size = 10):
     """
     Input : n_arms -> no. of arms
           : max_mean -> sets the mean range from 0 to max_mean || by default set to 10
+          : size -> no. of different values that can be taken from each arm
     Output : k_mean_dict -> Dictonary with key values as integer : normal distr. vals
     
     """
@@ -14,13 +15,16 @@ def create_enviroment(n_arms : int, max_mean = 10):
     assert max_mean, f"max_mean needs to be an integer NOT {max_mean}" 
     
     k_mean_dict = {}
-    keys = list(range(1, n_arms+1))
+
+    keys = list(range(0, n_arms))
+    
     values = []
-    for i in range(n_arms):#loc needs to be random a bit
+    
+    for _ in range(n_arms):#loc needs to be random a bit
         rand_val = random.randint(0, max_mean)#range b/w 0 to 10
-        curr_val = np.random.normal(loc = rand_val, scale = 1, size = 10)#normal dist. #size->no. of observations
+        curr_val = np.random.normal(0, 1, size = size)#normal dist. #size->no. of observations
         values.append(curr_val)
-        #filling dict
+        
     k_mean_dict = dict(zip(keys, values))
     return k_mean_dict
 

src/utils.py
@@ -1,35 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import csv
 import datetime
 
+import csv
 
 #Plot Epsilon-greedy comparison
-def plot_epsilon_greedy_comparison(reward_lists):
+def plot_epsilon_greedy_comparison(reward_lists, graph_title, y_label):
+    
     fig, ax = plt.subplots()
-
 
     for title, score in reward_lists.items():
         ax.plot(score, label=str(title))
+    
+    ax.set_ylabel(y_label)
+    ax.set_xlabel("Steps")
+    ax.set_title(graph_title)
+    ax.legend()
+
+
+    plt.show()
+
+def plot_rewards(reward_list, title):
+
+    fig, ax = plt.subplots()
+
+    scores = np.asarray(reward_list)
+    ax.plot(scores)
     ax.set_ylabel("Rewards")
     ax.set_xlabel("Steps")
-    ax.set_title("Epsilon Greedy Comparison Graph")
-    ax.legend()
+    ax.set_title(title)
     plt.show()
-
-def plot_rewards(reward_list, figsize = (1, 1)):
-    scores = np.asarray(reward_list)
-    plt.plot(scores)
-    plt.ylabel("Rewards")
-    plt.xlabel("Steps")
-    plt.title("EMPTY PASS")
-    plt.figure(figsize = figsize)
-    plt.show()
-
 
 
 def write_csv(filename, data):
+
+    #File path
     csv_file_path = "results/"+filename+"_"+str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))+'.csv'
+
+    #Data prep
+
+
+
+    #Data writing
     with open(csv_file_path, mode='w', newline='') as file:
         # Create a csv.writer object
         writer = csv.writer(file)